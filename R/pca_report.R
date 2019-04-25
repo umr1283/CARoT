6d35d7426c3c65261f87ceabe62bf51027c3ba9b
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2019-04-25 17:54:10 +0200

    remove require

--- R/pca_report.R
+++ R/pca_report.R
@@ -9,32 +9,28 @@ pca_report <- function(
   outliers_threshold = 3,
   title_level = 2
 ) {
-  require(flashpcaR)
-  require(scales)
-  require(tidyverse)
-  
   if (!methods::is(design, "data.frame")) {
     design <- as.data.frame(design)
   }
   if (!methods::is(design[, id_var], "character")) {
     design[[id_var]] <- as.character(design[[id_var]])
   }
-  
+
   keep_technical <- sapply(
-    X = design[, technical_vars, drop = FALSE], 
+    X = design[, technical_vars, drop = FALSE],
     FUN = function(icol) {
       length(unique(icol))>1 & length(unique(icol))!=length(design[, id_var])
     }
-  ) %>% 
-    which() %>% 
+  ) %>%
+    which() %>%
     names()
-  
+
   if (length(setdiff(technical_vars, keep_technical))!=0) {
     variables_excluded <- setdiff(technical_vars, keep_technical)
     message(
-      "The following variables have been excluded (null variances or confounding with samples): ", 
-        paste(variables_excluded[-length(variables_excluded)], collapse = ", "), 
-        " and ", 
+      "The following variables have been excluded (null variances or confounding with samples): ",
+        paste(variables_excluded[-length(variables_excluded)], collapse = ", "),
+        " and ",
         variables_excluded[length(variables_excluded)]
     )
   }
@@ -44,19 +40,19 @@ pca_report <- function(
     stand = "sd",
     ndim = n_comp
   )
-  
+
   pca_dfxy <- pca_res %>%
     `[[`("projection") %>%
     as.data.frame() %>%
     `colnames<-`(paste0("PC", seq_len(ncol(.)))) %>%
-    dplyr::mutate(Sample_ID = as.character(colnames(data))) %>% 
-    dplyr::left_join(x = design, y = ., by = id_var) %>% 
+    dplyr::mutate(Sample_ID = as.character(colnames(data))) %>%
+    dplyr::left_join(x = design, y = ., by = id_var) %>%
     as.data.frame()
 
 
   cat(paste0("\n", paste(rep("#", title_level), collapse = ""), " PCA inertia contribution {-}\n"))
   p <- tibble::tibble(
-    y = (pca_res$values / sum(pca_res$values)), 
+    y = (pca_res$values / sum(pca_res$values)),
     x = sprintf("PC%02d", seq_along(pca_res$values))
   ) %>%
     dplyr::mutate(cumsum = cumsum(y)) %>%
@@ -86,7 +82,7 @@ pca_report <- function(
         ggplot2::stat_ellipse(type = "norm") +
         ggplot2::scale_colour_viridis_d() +
         ggplot2::labs(x = NULL, y = NULL) +
-        ggplot2::facet_grid(rows = vars(Y.PC), cols = vars(X.PC), scales = "free") +
+        ggplot2::facet_grid(rows = vars(Y.PC), cols = ggplot2::vars(X.PC), scales = "free") +
         ggplot2::guides(colour = ifelse(length(unique(pca_dfxy[, ivar])) <= 12, "legend", "none"))
       print(p)
       cat("\n")
@@ -96,10 +92,10 @@ pca_report <- function(
     p <- pca_dfxy %>%
       (function(.data) {
         lapply(seq_len(fig_n_comp), function(i) {
-          form <- as.formula(paste0("PC", i, " ~ ", paste(keep_technical, collapse = " + ")))
-          lm(form, data = .data) %>% 
-            anova() %>% 
-            tibble::rownames_to_column(var = "term") %>% 
+          form <- ggplot2::vars(paste0("PC", i, " ~ ", paste(keep_technical, collapse = " + ")))
+          stats::lm(form, data = .data) %>%
+            stats::anova() %>%
+            tibble::rownames_to_column(var = "term") %>%
             dplyr::mutate(PC = i)
         }) %>%
           dplyr::bind_rows()
@@ -109,8 +105,8 @@ pca_report <- function(
       ggplot2::ggplot(mapping = ggplot2::aes(x = factor(PC), y = term, fill = `Pr(>F)`)) +
       ggplot2::geom_tile(colour = "white") +
       ggplot2::geom_text(
-        mapping = ggplot2::aes(label = scales::scientific(`Pr(>F)`, digits = 2)), 
-        colour = "white", 
+        mapping = ggplot2::aes(label = scales::scientific(`Pr(>F)`, digits = 2)),
+        colour = "white",
         size = 3
       ) +
       ggplot2::scale_fill_viridis_c(name = "P-Value", na.value = "grey85", limits = c(0, 0.1)) +
@@ -132,25 +128,25 @@ pca_report <- function(
       tibble::tibble(EuclideanDistance = .) %>%
       tibble::rownames_to_column(var = id_var) %>%
       dplyr::mutate(
-        BadSamplesLogical = 
-          EuclideanDistance <= 
-            (median(EuclideanDistance) - outliers_threshold * IQR(EuclideanDistance)) |
-          EuclideanDistance >= 
-            (median(EuclideanDistance) + outliers_threshold * IQR(EuclideanDistance)),
+        BadSamplesLogical =
+          EuclideanDistance <=
+            (stats::median(EuclideanDistance) - outliers_threshold * stats::IQR(EuclideanDistance)) |
+          EuclideanDistance >=
+            (stats::median(EuclideanDistance) + outliers_threshold * stats::IQR(EuclideanDistance)),
         BadSamples = factor(ifelse(BadSamplesLogical, "BAD", "GOOD"), levels = c("BAD", "GOOD"))
       ) %>%
       tibble::column_to_rownames(var = id_var)
-  
+
     pca_dfxy <- merge(
       x = pca_dfxy,
       y = pca_outliers,
       by = "row.names"
     ) %>%
       tibble::column_to_rownames(var = "Row.names")
-    
+
     ivar <- "BadSamples"
-    
-    p <- dplyr::bind_rows(apply(t(combn(paste0("PC", seq_len(fig_n_comp)), 2)), 1, function(icoord) {
+
+    p <- dplyr::bind_rows(apply(t(utils::combn(paste0("PC", seq_len(fig_n_comp)), 2)), 1, function(icoord) {
       tmp <- pca_dfxy[, c(ivar, icoord)]
       tmp[, ivar] <- as.factor(tmp[, ivar])
       colnames(tmp)[-seq_len(ncol(tmp) - 2)] <- c("X", "Y")
@@ -165,14 +161,14 @@ pca_report <- function(
       ggplot2::stat_ellipse(type = "norm") +
       ggplot2::scale_colour_viridis_d() +
       ggplot2::labs(x = NULL, y = NULL) +
-      ggplot2::facet_grid(rows = vars(Y.PC), cols = vars(X.PC), scales = "free")
+      ggplot2::facet_grid(rows = ggplot2::vars(Y.PC), cols = ggplot2::vars(X.PC), scales = "free")
     print(p)
     cat("\n")
-    
+
     # pca_dfxy <- pca_dfxy %>%
     #   dplyr::select(-dplyr::starts_with("PC")) %>%
     #   dplyr::filter(BadSamples == "BAD")
   }
-  
+
   invisible(pca_dfxy)
 }
