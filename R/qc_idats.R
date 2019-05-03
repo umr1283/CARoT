8ceba1ed6ce4a6c118e7d52ee51d93b89510ab7a
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2019-05-03 16:59:56 +0200

    add template and function to QC idats

---
+++ R/qc_idats.R
@@ -0,0 +1,115 @@
+#' qc_idats
+#'
+#' @param csv_file A `character`.
+#' @param data_directory A `character`.
+#' @param output_file A `character`.
+#' @param output_directory A `character`.
+#' @param array A `character`.
+#' @param annotation A `character`.
+#' @param filter_snps A `logical`.
+#' @param filter_non_cpg A `logical`.
+#' @param filter_xy A `logical`.
+#' @param filter_multihit A `logical`.
+#' @param filter_beads A `logical`.
+#' @param population A `character`.
+#' @param bead_cutoff A `numeric`.
+#' @param threshold_detection_pvalues A `numeric`.
+#' @param threshold_call_rate_samples A `numeric`.
+#' @param threshold_call_rate_cpgs A `numeric`.
+#' @param threshold_gender A `numeric`.
+#' @param colname_gender A `character`.
+#' @param norm_background A `character`.
+#' @param norm_dye A `character`.
+#' @param norm_quantile A `character`.
+#' @param cell_tissue A `character`.
+#' @param pca_vars A `vector(character)`.
+#' @param title A `character`.
+#' @param author_name A `character`.
+#' @param author_affiliation A `character`.
+#' @param author_email A `character`.
+#' @param show_code A `logical`.
+#' @param n_cores A `numeric`.
+#' @param dpi A `numeric`.
+#' @param gg_fontsize A `numeric`.
+#' @param encoding A `character`.
+#'
+#' @return NULL
+#' @export
+qc_idats <- function(
+  csv_file,
+  data_directory,
+  output_file = "EPIC_QC.html",
+  output_directory = ".",
+  array = "EPIC",
+  annotation = "ilm10b4.hg19",
+  filter_snps = TRUE,
+  filter_non_cpg = TRUE,
+  filter_xy = TRUE,
+  filter_multihit = TRUE,
+  filter_beads = TRUE,
+  population = NULL,
+  bead_cutoff = 0.05,
+  threshold_detection_pvalues = 0.01,
+  threshold_call_rate_samples = 0.99,
+  threshold_call_rate_cpgs = 1,
+  threshold_gender = -2,
+  colname_gender = "sexe",
+  norm_background = "oob",
+  norm_dye = "RELIC",
+  norm_quantile = "quantile1",
+  cell_tissue = NULL,
+  pca_vars = c("Sample_Plate", "Sentrix_ID"),
+  title = "EPIC Array Quality-Control",
+  author_name = "CARoT",
+  author_affiliation = "",
+  author_email = "",
+  show_code = FALSE,
+  n_cores = 20,
+  dpi = 300,
+  gg_fontsize = 12,
+  encoding = "UTF-8"
+) {
+  file.copy(
+    from = system.file("rmarkdown", "qc_idats.Rmd", package = "CARoT"),
+    to = paste0(tempdir(), "/qc_idats.Rmd"),
+    overwrite = TRUE
+  )
+  rmarkdown::render(
+    input = paste0(tempdir(), "/qc_idats.Rmd"),
+    output_file = output_file,
+    output_dir = output_directory,
+    encoding = encoding,
+    params = list(
+      title = title,
+      author_name = author_name,
+      author_affiliation = author_affiliation,
+      author_email = author_email,
+      output_directory = output_directory,
+      show_code = show_code,
+      n_cores = n_cores,
+      dpi = dpi,
+      gg_fontsize = gg_fontsize,
+      csv_file = csv_file,
+      data_directory = data_directory,
+      array = array,
+      annotation = annotation,
+      filter_snps = filter_snps,
+      filter_non_cpg = filter_non_cpg,
+      filter_xy = filter_xy,
+      filter_multihit = filter_multihit,
+      filter_beads = filter_beads,
+      population = population,
+      bead_cutoff = bead_cutoff,
+      threshold_detection_pvalues = threshold_detection_pvalues,
+      threshold_call_rate_samples = threshold_call_rate_samples,
+      threshold_call_rate_cpgs = threshold_call_rate_cpgs,
+      threshold_gender = threshold_gender,
+      colname_gender = colname_gender,
+      norm_background = norm_background,
+      norm_dye = norm_dye,
+      norm_quantile = norm_quantile,
+      cell_tissue = cell_tissue,
+      pca_vars = pca_vars
+    )
+  )
+}
