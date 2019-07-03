ea10033cbfe1a916d39c9b6ab58c69b96d75ccf7
Mickaël Canouil <mickael.canouil@cnrs.fr>
2019-07-03 14:22:50 +0200

    update doc

--- _build.sh
+++ _build.sh
@@ -1,12 +1,24 @@
-docker exec R360 Rscript -e 'remotes::install_github("mcanouil/CARoT", upgrade = "never")'
+#!/bin/sh
 
+# docker exec R360 Rscript -e 'remotes::install_github("mcanouil/CARoT", upgrade = "never")'
 
-devtools::check()
-system("git checkout master")
-pkgdown::build_site(override = list(destination = tempdir()), preview = FALSE)
-system("git checkout gh-pages")
-file.copy(from = list.files(paste0(tempdir(), "/dev"), full.names = TRUE), to = ".", recursive = TRUE, overwrite = TRUE)
-system('git add --all; git -c "user.name=Mickaël Canouil" -c "user.email=mickael.canouil@cnrs.fr" commit -m "Built site for CARoT: 0.3.0.9000"')
-unlink(paste(tempdir(), "/dev"), recursive = TRUE)
-system("git checkout master")
+Rscript -e 'devtools::check()'
+
+git checkout master
+
+Rscript -e 'pkgdown::build_site(override = list(destination = tempdir()), preview = FALSE)')
+
+git checkout gh-pages
+
+Rscript -e 'file.copy(from = list.files(paste0(tempdir(), "/dev"), full.names = TRUE), to = ".", recursive = TRUE, overwrite = TRUE)')
+
+git add --all
+
+Rscript -e 'Sys.setenv(pkg_commit = paste("Built site for CARoT:", pkgdown:::read_desc()$get_version()))'
+git -c "user.name=Mickaël Canouil" -c "user.email=mickael.canouil@cnrs.fr" commit -m $pkg_commit
+
+Rscript -e 'unlink(paste(tempdir(), "/dev"), recursive = TRUE)')
+
+git checkout master
+
 
