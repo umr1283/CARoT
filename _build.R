46be3c7a62c826ae74b9bafffd3b737a8cab622d
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2019-07-03 14:28:39 +0200

    change build script

---
+++ _build.R
@@ -0,0 +1,14 @@
+#!/usr/local/bin/Rscript
+
+# docker exec R360 Rscript -e 'remotes::install_github("mcanouil/CARoT", upgrade = "never")'
+
+devtools::check()
+system("git checkout master")
+pkgdown::build_site(override = list(destination = tempdir()), preview = FALSE)
+system("git checkout gh-pages")
+file.copy(from = list.files(paste0(tempdir(), "/dev"), full.names = TRUE), to = ".", recursive = TRUE, overwrite = TRUE)
+system("git add --all")
+commit <- paste('Built site for CARoT:', pkgdown:::read_desc()$get_version())
+system(paste("git -c "user.name=Mickaël Canouil" -c "user.email=mickael.canouil@cnrs.fr" commit -m", commit))
+unlink(paste(tempdir(), "/dev"), recursive = TRUE)
+system("git checkout master")
