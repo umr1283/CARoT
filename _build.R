f55583c0347e11719729de8085f2473ad34968bc
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2019-07-22 12:36:43 +0200

    full R build

--- _build.R
+++ _build.R
@@ -2,13 +2,16 @@
 
 # docker exec R360 Rscript -e 'remotes::install_github("mcanouil/CARoT", upgrade = "never")'
 
-devtools::check()
-system("git checkout master")
+# git2r::config(user.name = "Mickaël Canouil", user.email = "mickael.canouil@cnrs.fr")
+# use_git_credentials(credentials = git2r::cred_token())
+check()
+git2r::checkout(branch = "master")
 commit <- paste('Built site for CARoT:', pkgdown:::read_desc()$get_version())
 pkgdown::build_site(override = list(destination = tempdir()), preview = FALSE)
-system("git checkout gh-pages")
+git2r::checkout(branch = "gh-pages")
 file.copy(from = list.files(paste0(tempdir(), "/dev"), full.names = TRUE), to = ".", recursive = TRUE, overwrite = TRUE)
-system("git add --all")
-system(paste0('git -c "user.name=Mickaël Canouil" -c "user.email=mickael.canouil@cnrs.fr" commit -m "', commit, '"'))
-unlink(paste(tempdir(), "/dev"), recursive = TRUE)
-system("git checkout master")
+git2r::add(path = "*")
+git2r::commit(message = commit, all = TRUE)
+git2r::checkout(branch = "master")
+git2r::push(name = "origin", refspec = "refs/heads/master", credentials = git2r::cred_token())
+git2r::push(name = "origin", refspec = "refs/heads/gh-pages", credentials = git2r::cred_token())
