7106813ab1b6676af9e6a1c701882961ef8646fb
Mickaël Canouil <mickael.canouil@cnrs.fr>
2019-08-05 15:44:28 +0200

    fix #4 condition length issue

--- R/estimate_ethnicity.R
+++ R/estimate_ethnicity.R
@@ -186,18 +186,21 @@ check_input <- function(input) {
   message_prefix <- "[CARoT] "
 
   name <- deparse(substitute(input))
-  if (!fs::is_dir(input) & !fs::is_file(input)) {
+  is_input_good <- (length(input) == 1 && (fs::is_dir(input) | fs::is_file(input))) |
+    all(fs::is_file(input))
+
+  if (!is_input_good) {
     stop(
       message_prefix, 'A valid "', name, '" must be provided, ',
-      'either a directory (with VCF files) or a vcf file!'
+      'either a directory (with VCF files) or vcf file(s)!'
     )
   }
-  if (fs::is_dir(input)) {
+  if (length(input) == 1 && fs::is_dir(input)) {
     list_input <- list.files(path = input, pattern = ".vcf.gz$", full.names = TRUE)
   } else {
     list_input <- input
   }
-  if (!all(grepl(".vcf.gz$", list_input))) {
+  if (!all(grepl(".vcf.gz$", list_input) & fs::is_file(list_input))) {
     stop(message_prefix, 'VCF files must be compressed using bgzip!')
   }
   if (length(list_input)==0) {
